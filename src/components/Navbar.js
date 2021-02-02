f9eb5b6a1a5b272133f29e3f7f24d2cc52ccff29
michael-mcmasters <jmcmasters411@gmail.com>
2021-02-02 10:04:04 -0500

    Create regular CSS file

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,32 @@
 import React from 'react';
 import styled from 'styled-components';
+import "./css/Navbar.css";
 
 const Navbar = () => {
   return (
     <div>
-      <div style={{ backgroundColor: "green" }}>
-        hello
-      </div>
-      <Temp>aye</Temp>
+      <Container>
+        Icon
+      </Container>
+      <div className="text">yoooo</div>
     </div>
   );
 };
 
 export default Navbar;
 
+const Container = styled.div`
+  background-color: red;
+  padding: 2rem;
+`;
+
+const SearchBar = styled.div`
+  
+`;
+
 const Temp = styled.div`
   background-color: red;
+  &:hover {
+    background-color: blue;
+  }
 `;
