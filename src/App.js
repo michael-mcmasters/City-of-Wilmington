77913bf6355352aab1fa1166c3470e8d697df02c
michael-mcmasters <jmcmasters411@gmail.com>
2021-02-02 11:33:49 -0500

    Create SideHoverBar component and template info

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import logo from './logo.svg';
 import Navbar from "./components/Navbar";
+import SideHoverbar from "./components/SideHoverbar";
 import cityImg from "./images/davisisland.jpg";   // Need to import the image here for webpack to work
 import './App.css';
 
 function App() {
   return (
     <div className="main-color-theme">
-      <Navbar></Navbar>
+      <Navbar />
       <div className="image-container">
         <img src={cityImg} alt="A tree on Davis Island overlooking the water" />
       </div>
+      <SideHoverbar />
     </div>
   );
 }
---
+++ src/components/SideHoverbar.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const SideHoverBar = () => {
+  return (
+    <div>
+      <div>
+        Make a Payment
+      </div>
+      <div>
+        Utilities
+      </div>
+      <div>
+        Jobs &amp; Careers
+      </div>
+      <div>
+        Master Plans
+      </div>
+      <div>
+        Trash &amp; Recycling
+      </div>
+      <div>
+        GIS Maps
+      </div>
+      <div>
+        Animal Services
+      </div>
+      <div>
+        Parks and Recreation
+      </div>
+    </div>
+  );
+};
+
+export default SideHoverBar;
