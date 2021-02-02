fce03ef0ab78a46af2f995f9c92bd5dfa4b11a4f
michael-mcmasters <jmcmasters411@gmail.com>
2021-02-02 10:10:12 -0500

    Get started on helper CSS classes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,28 +5,34 @@ import "./css/Navbar.css";
 const Navbar = () => {
   return (
     <div>
-      <Container>
-        Icon
-      </Container>
-      <div className="text">yoooo</div>
+      <div className="container">
+        <div class="d-flex space-btwn">
+          <span>Icon</span>
+          <input type="text"></input>
+        </div>
+        <div className="ml-auto">
+          <span>Government</span>
+          <span>Community</span>
+        </div>
+      </div>
     </div>
   );
 };
 
 export default Navbar;
 
-const Container = styled.div`
-  background-color: red;
-  padding: 2rem;
-`;
+// const Container = styled.div`
+//   background-color: red;
+//   padding: 2rem;
+// `;
 
-const SearchBar = styled.div`
-  
-`;
+// const SearchBar = styled.div`
 
-const Temp = styled.div`
-  background-color: red;
-  &:hover {
-    background-color: blue;
-  }
-`;
+// `;
+
+// const Temp = styled.div`
+//   background-color: red;
+//   &:hover {
+//     background-color: blue;
+//   }
+// `;
