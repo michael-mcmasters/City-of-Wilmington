b88938fa0d1b340f80d40d85fb4f6e7f0897b7b1
michael-mcmasters <jmcmasters411@gmail.com>
2021-02-02 14:19:44 -0500

    Remove margin at edges of window. Add new component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import logo from './logo.svg';
 import Navbar from "./components/Navbar";
 import SideHoverbar from "./components/SideHoverbar";
+import ServiceCenter from "./components/ServiceCenter";
 import cityImg from "./images/davisisland.jpg";   // Need to import the image here for webpack to work
 import './App.css';
 
 function App() {
   return (
-    <div className="main-color-theme">
+    <div className="main-color-theme m-0">
       <SideHoverbar />
       <Navbar />
       <div className="image-container">
         <img src={cityImg} alt="A tree on Davis Island overlooking the water" />
       </div>
+      <ServiceCenter />
     </div>
   );
 }
