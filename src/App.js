5f8f27d777a1a1ab27df8cc8b5e292639e619219
michael-mcmasters <jmcmasters411@gmail.com>
2021-02-02 10:58:06 -0500

    Display image on page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import Navbar from "./components/Navbar";
+import cityImg from "./images/davisisland.jpg";   // Need to import the image here for webpack to work
 import './App.css';
 
 function App() {
   return (
     <div className="main-color-theme">
       <Navbar></Navbar>
+      <img src={cityImg} alt="fdsa" />
     </div>
   );
 }
