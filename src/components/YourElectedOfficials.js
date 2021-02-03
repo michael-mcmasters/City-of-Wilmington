023328bd4700ef6bf71dbe9cd2567421bb491538
michael-mcmasters <jmcmasters411@gmail.com>
2021-02-03 13:46:00 -0500

    Create functions to display different layouts

--- src/components/YourElectedOfficials.js
+++ src/components/YourElectedOfficials.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Avatar from "./Avatar";
 import img from "../images/card-images/20160921_161617.jpg";
 import "./css/YourElectedOfficials.css";
 
 const YourElectedOfficials = () => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
   const data = getElectedOfficialsData();
+
+  useEffect(() => {
+    window.addEventListener('resize', () => setWindowWidth(window.innerWidth))
+    console.log("re-rendering")
+  }, [windowWidth])
 
   // Using this instead of map so that we can control what rows the cards are displayed in.
   const getAvatar = (i) => {
@@ -16,29 +22,64 @@ const YourElectedOfficials = () => {
     />
   }
 
+  const getLayoutOne = () => {
+    return (
+      <>
+        <div className="d-flex space-center">
+          {getAvatar(0)}
+          {getAvatar(1)}
+          {getAvatar(2)}
+          {getAvatar(3)}
+        </div>
+        <div className="d-flex space-center">
+          {getAvatar(3)}
+        </div>
+        <div className="d-flex space-center">
+          {getAvatar(6)}
+          {getAvatar(7)}
+          {getAvatar(8)}
+          {getAvatar(8)}
+        </div>
+        <div className="d-flex space-center">
+          {getAvatar(6)}
+          {getAvatar(7)}
+          {getAvatar(8)}
+        </div>
+      </>
+    );
+  }
+
+  const getLayoutTwo = () => {
+    return (
+      <>
+        <div className="d-flex space-center">
+          {getAvatar(0)}
+          {getAvatar(1)}
+          {getAvatar(2)}
+          {getAvatar(3)}
+        </div>
+        <div className="d-flex space-center">
+          {getAvatar(3)}
+        </div>
+        <div className="d-flex space-center">
+          {getAvatar(6)}
+          {getAvatar(7)}
+          {getAvatar(8)}
+          {getAvatar(8)}
+        </div>
+        <div className="d-flex space-center">
+          {getAvatar(6)}
+          {getAvatar(7)}
+          {getAvatar(8)}
+        </div>
+      </>
+    );
+  }
+
   return (
     <div className="mt-3 edge-screen-margin text-center elected-officials-container">
       <h2 className="service-center-txt">Your Elected Officials</h2>
-      <div className="d-flex space-center">
-        {getAvatar(0)}
-        {getAvatar(1)}
-        {getAvatar(2)}
-        {getAvatar(3)}
-      </div>
-      <div className="d-flex space-center">
-        {getAvatar(3)}
-      </div>
-      <div className="d-flex space-center">
-        {getAvatar(6)}
-        {getAvatar(7)}
-        {getAvatar(8)}
-        {getAvatar(8)}
-      </div>
-      <div className="d-flex space-center">
-        {getAvatar(6)}
-        {getAvatar(7)}
-        {getAvatar(8)}
-      </div>
+      { getLayoutOne()}
     </div>
   );
 };
